87cc1d4625e7f88e3a6ced8cdf8da17f328c6a93
bober <brian.familybrian123@gmail.com>
2024-03-20 23:28:55 +1100

    typo lol

--- app/boards/shields/nice_view/widgets/art.c
+++ app/boards/shields/nice_view/widgets/art.c
@@ -232,8 +232,8 @@ const lv_img_dsc_t mountain = {
 #define LV_ATTRIBUTE_IMG_EYE_TEST
 #endif
 
-const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_EYE_TEST uint8_t eye
-    test_map[] = {
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_EYE_TEST uint8_t 
+        eye_test_map[] = {
 #if CONFIG_NICE_VIEW_WIDGET_INVERTED
         0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
         0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
@@ -327,12 +327,12 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_EYE_TEST 
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 };
 
-const lv_img_dsc_t eye test = {
+const lv_img_dsc_t eye_test = {
     .header.cf = LV_IMG_CF_INDEXED_1BIT,
     .header.always_zero = 0,
     .header.reserved = 0,
     .header.w = 68,
     .header.h = 140,
     .data_size = 1268,
-    .data = eye test_map,
+    .data = eye_test_map,
 };
